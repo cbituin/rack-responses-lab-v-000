ff0c2208c673a9c46648e63bed34633641acb299
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-06-06 04:11:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,14 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    currentTime = Time.now
+    t = Time.now
     
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
- 
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
+    resp.write "#{t}"
+    
+    if t.hour >= 0 && t.hour < 12
+      resp.write "Good morning!"
     else
-      resp.write "You Lose"
+      resp.write "Good Afternoon!"
     end
  
     resp.finish
